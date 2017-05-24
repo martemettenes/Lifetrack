b86cd41a21e0eb0c7c9a68cfd22f0541c37612d3
Marte <Marte@Martes-MacBook-Pro-3.local>
2017-05-24 19:30:56 +0200

    Layout
    Completely new layout

--- main.js
+++ main.js
@@ -58,3 +58,18 @@ createWindow();
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+
+// FUNCTIONS WITHIN THE APP //
+
+
+localStorage.setItem("listName", "some text");
+
+var data = localStorage.getItem("listName");
+console.log(data);
+
+
+
+
+
